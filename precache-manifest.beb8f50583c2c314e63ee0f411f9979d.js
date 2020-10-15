40b46ba789d7dfb3328e9efea321da927192d14f
LeeWannacott <wannacottL@gmail.com>
2020-10-16 12:53:24 +1300

    Updates

---
+++ precache-manifest.beb8f50583c2c314e63ee0f411f9979d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c601a0e4e9e60c6e33270c2952306031",
+    "url": "/%7BPortfolio%7D/index.html"
+  },
+  {
+    "revision": "81fb6fda94ea7f6b6e2b",
+    "url": "/%7BPortfolio%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf44fe75a53e89d13080",
+    "url": "/%7BPortfolio%7D/static/js/2.c80743ad.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/%7BPortfolio%7D/static/js/2.c80743ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81fb6fda94ea7f6b6e2b",
+    "url": "/%7BPortfolio%7D/static/js/main.fa2dd95a.chunk.js"
+  },
+  {
+    "revision": "d4ded6a0e2e320ccfbcc",
+    "url": "/%7BPortfolio%7D/static/js/runtime-main.6c44731f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7BPortfolio%7D/static/media/logo.5d5d9eef.svg"
+  }
+]);
