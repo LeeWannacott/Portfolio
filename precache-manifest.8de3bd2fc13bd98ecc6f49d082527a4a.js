6558913b87b30bb957432d3a43e1f1152f30550f
LeeWannacott <WannacottL@gmail.com>
2023-06-11 12:10:30 +1200

    Updates

--- precache-manifest.8de3bd2fc13bd98ecc6f49d082527a4a.js
+++ precache-manifest.8de3bd2fc13bd98ecc6f49d082527a4a.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "688c673fe82940f4472b621e794db2f3",
+    "revision": "8e6ec0352fca8440b83932c7575174bd",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "6ad5503a564edbfa4812",
+    "revision": "87f312f1e43e978e3988",
     "url": "/Portfolio/static/css/2.a34399e1.chunk.css"
   },
   {
-    "revision": "91c22087024f30615f77",
+    "revision": "00314436be0f57d5d257",
     "url": "/Portfolio/static/css/main.73d637fc.chunk.css"
   },
   {
-    "revision": "6ad5503a564edbfa4812",
-    "url": "/Portfolio/static/js/2.ad9a85ae.chunk.js"
+    "revision": "87f312f1e43e978e3988",
+    "url": "/Portfolio/static/js/2.bc67827a.chunk.js"
   },
   {
     "revision": "a70c635ba6df9a3236ab9fa4e81f6f89",
-    "url": "/Portfolio/static/js/2.ad9a85ae.chunk.js.LICENSE.txt"
+    "url": "/Portfolio/static/js/2.bc67827a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "91c22087024f30615f77",
-    "url": "/Portfolio/static/js/main.90e19e01.chunk.js"
+    "revision": "00314436be0f57d5d257",
+    "url": "/Portfolio/static/js/main.01ce222a.chunk.js"
   },
   {
     "revision": "9ca9e1894f14f4a03125",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.b1a695f0e29cc2a6143f48a483e10bb6.js"
+  "/Portfolio/precache-manifest.8de3bd2fc13bd98ecc6f49d082527a4a.js"
 );
 
 self.addEventListener('message', (event) => {
